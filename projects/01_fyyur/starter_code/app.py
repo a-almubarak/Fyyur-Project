b5d2014009b04c2ada1a02cbbe73847b1f966914
iabdull4h <euh.27@hotmail.com>
2021-06-25 19:16:38 +0300

    added app.cofing, flask migrate

--- projects/01_fyyur/starter_code/app.py
+++ projects/01_fyyur/starter_code/app.py
@@ -12,6 +12,7 @@ import logging
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *
+from flask_migrate import Migrate
 #----------------------------------------------------------------------------#
 # App Config.
 #----------------------------------------------------------------------------#
@@ -20,9 +21,11 @@ app = Flask(__name__)
 moment = Moment(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+migrate = Migrate(app,db)
 
 # TODO: connect to a local postgresql database
-
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost:5434/fyyur'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False #to supress the notification
 #----------------------------------------------------------------------------#
 # Models.
 #----------------------------------------------------------------------------#

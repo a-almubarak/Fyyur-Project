3a42e60093ac31979173a77bb5e6d7e18db9e01d
iabdull4h <euh.27@hotmail.com>
2021-07-08 08:35:32 +0300

    seperated the moedls to diff. file.

---
+++ projects/01_fyyur/starter_code/models.py
@@ -0,0 +1,54 @@
+from app import db
+class Venue(db.Model):
+    __tablename__ = "Venue"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String,nullable=False)
+    city = db.Column(db.String(120),nullable=False)
+    state = db.Column(db.String(120),nullable=False)
+    address = db.Column(db.String(120),nullable=False)
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String(120)),nullable=False)
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(120))
+    seeking_talent = db.Column(db.Boolean)
+    seeking_description = db.Column(db.String())
+
+    artists = db.relationship("Show", back_populates="venue")
+
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+
+
+class Artist(db.Model):
+    __tablename__ = "Artist"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String,nullable=False)
+    city = db.Column(db.String(120),nullable=False)
+    state = db.Column(db.String(120),nullable=False)
+    phone = db.Column(db.String(120))
+    genres = db.Column(db.ARRAY(db.String(120)),nullable=False)
+    image_link = db.Column(db.String(500))
+    facebook_link = db.Column(db.String(120))
+    website_link = db.Column(db.String(120))
+    seeking_venue = db.Column(db.Boolean())
+    seeking_description = db.Column(db.String())
+
+    venues = db.relationship("Show", back_populates="artist")
+
+    # TODO: implement any missing fields, as a database migration using Flask-Migrate
+
+
+# TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+class Show(db.Model):
+    __tablename__ = "Show"
+    id = db.Column(db.Integer, primary_key=True)
+    artist_id = db.Column(db.Integer, db.ForeignKey("Artist.id",ondelete='CASCADE',onupdate='CASCADE'))
+    venue_id = db.Column(db.Integer, db.ForeignKey("Venue.id",ondelete='CASCADE',onupdate='CASCADE'))
+    start_time = db.Column(db.String, nullable=False)
+
+    artist = db.relationship("Artist", back_populates="venues")
+    venue = db.relationship("Venue", back_populates="artists")
+
+
